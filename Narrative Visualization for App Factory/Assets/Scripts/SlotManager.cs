13459ccc2553bd38369f026cf860a13ee146080e
Soooolenia <samtsai3731@gmail.com>
2026-06-20 22:01:02 +0100

    Slot Prefab update

--- Narrative Visualization for App Factory/Assets/Scripts/SlotManager.cs
+++ Narrative Visualization for App Factory/Assets/Scripts/SlotManager.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SlotManager : MonoBehaviour
 {
-    
+    [SerializeField] private TMP_Text textComponent;
+    [SerializeField] private Button eraseButton;
+    [SerializeField] private Button replaceButton;
+
+    public ClueType ClueType { get; private set; } = ClueType.None;
+    public bool IsEmpty => textComponent.text == "";
+    public string Description => textComponent.text;
+
+    public void Write(string description, ClueType clueType)
+    {
+        textComponent.text = description;
+        ClueType = clueType;
+    }
+
+    public void Clear()
+    {
+        textComponent.text = "";
+        ClueType = ClueType.None;
+    }
+
+    public void SetEraseButtonActive(bool active) => eraseButton.gameObject.SetActive(active);
+    public void SetReplaceButtonActive(bool active) => replaceButton.gameObject.SetActive(active);
 }
