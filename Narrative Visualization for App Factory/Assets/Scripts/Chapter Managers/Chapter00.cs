c347680d0e24fd32dc3af7b1dce5fbd23dc21674
Soooolenia <samtsai3731@gmail.com>
2026-06-25 13:33:23 +0100

    Menu Button Initial Code Implemented

---
+++ Narrative Visualization for App Factory/Assets/Scripts/Chapter Managers/Chapter00.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Chapter00 : MonoBehaviour
+{
+    [SerializeField] private CameraControl cameraControl;
+    private void Start()
+    {
+        cameraControl.enabled = false;
+    }
+}
