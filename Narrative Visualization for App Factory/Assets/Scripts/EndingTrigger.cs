5c1abd585735c590a1118604c98bb5293e076510
Soooolenia <samtsai3731@gmail.com>
2026-07-06 11:38:52 +0100

    Ending Sequence Map Bounds Lock Implemented

---
+++ Narrative Visualization for App Factory/Assets/Scripts/EndingTrigger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EndingTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject lastBound;
+    [SerializeField] private GameObject endingBound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lastBound.SetActive(false);
+        endingBound.SetActive(true);
+        Debug.Log("Ending Sequence Started!");
+    }
+}
